966d25f56fb9c3dbd7130c6527b9eb2339be94c6
Gabriela Soares <gabrielayuzu@gmail.com>
2022-07-17 10:08:28 -0400

    class livro

--- src/br/com/dio/primeiroPrograma.java
+++ src/br/com/dio/primeiroPrograma.java
@@ -7,8 +7,46 @@ public class primeiroPrograma {
         gato Gato = new gato();
 
         System.out.println(Gato);
+
+        Livro livro1 = new Livro("Mulheres que correm com lobos", 140);
+        System.out.println(livro1);
+        
         /*int a = 5;
         int b = 10;
         System.out.println("Hello World! " + (a+b));*/
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
