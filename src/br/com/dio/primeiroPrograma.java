e9d5ceb9e040aaf50381a99355b845da1140eb99
Gabriela Soares <gabrielayuzu@gmail.com>
2022-07-17 09:48:02 -0400

    first commit intellij

---
+++ src/br/com/dio/primeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import model.gato;
+
+public class primeiroPrograma {
+    public static void main(String[] args) {
+        gato Gato = new gato();
+
+        System.out.println(Gato);
+        /*int a = 5;
+        int b = 10;
+        System.out.println("Hello World! " + (a+b));*/
+    }
+}
